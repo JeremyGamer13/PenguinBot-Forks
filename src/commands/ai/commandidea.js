a5fafb73afeaceb7d6301f1ed7d262b257684880
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2026-03-21 16:11:03 -0600

    Create commandidea.js

---
+++ src/commands/ai/commandidea.js
@@ -0,0 +1,50 @@
+const ChatGPT = require("../../util/chatgpt");
+const ChatGPTClient = new ChatGPT();
+
+class Command {
+    constructor() {
+        this.name = "commandidea";
+        this.description = "I Need an idea for a command";
+        this.attributes = {
+            permission: 0,
+            lockedToCommands: false,
+            adminInclusive: ['860531746294726736', '790782926785609728', '567307285324496897'],
+        };
+    }
+
+    async invoke(message, args, util) {
+        if (args.length > 0) return message.reply("Shut up");
+        
+        // start asking chattus geepitus
+        const chatId = `aicommandidea-${Math.random()}`;
+        ChatGPTClient.createChat(chatId);
+        ChatGPTClient.informChat(chatId,
+            `You are a Discord bot named Jeremy Stream Bot.`
+            + `\n` + `Think of a really stupid discord bot command using general online internet humor (not memes).`
+            + `\n` + `You can be sarcastic, satirical and use irony. Avoid internet memes.`
+            + `\n` + `Keep your responses appropriate and inoffensive.`
+            + `\n` + `Keep your reasons in a short sentence. Never refer to yourself specifically to any extent.`
+        );
+
+        // get the response & reset the chat
+        let response = "";
+        try {
+            response = await ChatGPTClient.advancedPrompt(chatId, "Give me a really stupid discord bot command");
+        } catch (err) {
+            message.reply("**Took too long to prompt.** If this happens frequently then Ollama is probably not open on my PC right now");
+        }
+        ChatGPTClient.removeChat(chatId);
+        message.reply({
+            content: response.trim(),
+            allowedMentions: {
+                parse: [],
+                users: [],
+                roles: [],
+                repliedUser: true
+            }
+        });
+    }
+}
+
+// needs to do new Command() in index.js because typing static every time STINKS!
+module.exports = Command;
