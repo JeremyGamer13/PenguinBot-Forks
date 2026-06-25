0081109b39aa0ca43eaa1aa7c48397511d89628a
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2026-06-25 02:14:05 -0600

    Update restartr.js

--- src/commands/dev/restartr.js
+++ src/commands/dev/restartr.js
@@ -9,6 +9,12 @@ class Command {
         this.alias = ["restat", "restar", "resart", "restrt"];
     }
 
+    reject(message) {
+        message.reply({
+            content: 'You need a permission level of 529999999999999890890890890890890890389432497325823528392943 to run this command, yours is currently 0.'
+        });
+    }
+
     invoke(message) {
         message.reply(`haha spelt it wrong idiot`);
     }
