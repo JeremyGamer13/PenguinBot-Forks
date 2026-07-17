d8869945e7ca59360c4684956954a6ed3f9512de
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2026-07-17 01:34:15 -0600

    move everything Pmgpt related in JSB to here

--- src/util/ollama-models.js
+++ src/util/ollama-models.js
@@ -21,7 +21,7 @@ class OllamaModels {
     // constant configs (settings dont change on runtime)
     /** @type {Ollama.ChatRequest} */
     static penguinGPT = {
-        model: "custom-penguinmod-server-v3",
+        model: "custom-penguinmod-server-v4",
         think: false,
         timeout: 1.5 * 60 * 1000, // 1.5 minute
     };
