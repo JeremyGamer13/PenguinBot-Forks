bf6adcdeb3987469c5b9cd11c8200103d51ff439
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2026-08-11 01:42:03 -0600

    Update sekaisticker.js

--- src/commands/sekaisticker.js
+++ src/commands/sekaisticker.js
@@ -1,12 +1,16 @@
 const fs = require("fs");
 const path = require("path");
 
+const cssColor = require("@asamuzakjp/css-color");
+const { Jimp, JimpMime } = require("jimp");
+
 const env = require("../util/env-util.js");
+const tryCatch = require("../util/try-catch.js");
 
 class Command {
     constructor() {
         this.name = "sekaisticker";
-        this.description = "Caption a sticker of a project sekai chibi.";
+        this.description = "Caption a sticker of a Project SEKAI chibi.";
         this.attributes = {
             unlisted: true, // TODO: temp
             permission: 0,
@@ -21,17 +25,26 @@ class Command {
         this.configured = !!sekaiPath;
         if (this.configured) {
             const stickersJsonPath = path.join(sekaiPath, "src/characters.json");
-            this.stickers = JSON.parse(fs.readFileSync(stickersJsonPath, "utf8"));
+            const stickers = JSON.parse(fs.readFileSync(stickersJsonPath, "utf8"));
 
-            // list of valid chars
+            // list of valid chars & sticker variants
             this.colors = {};
+            this.stickers = {};
             this.characters = new Set();
             for (const sticker of stickers) {
-                if (this.characters.has(sticker.character))
+                const characterName = `${sticker.character}`.toLowerCase().replace(/\s/g, "").trim();
+                if (this.characters.has(characterName))
                     continue;
-                this.characters.add(sticker.character);
-                this.colors[sticker.character] = sticker.color;
+                this.characters.add(characterName);
+                this.colors[characterName] = sticker.color;
+                this.stickers[characterName] = sticker;
             }
+
+            // make the long description from the list of characters
+            this.descriptionLong = "Caption a sticker of a Project SEKAI chibi."
+                + "\n" + "Either specify a character's name, or attach your own image to caption."
+                + "\n" + "When captioning your own image, provide a hex color rather than a character name."
+                + "\n" + "The valid characters are:" + " " + Array.from(this.characters.values()).map(name => `\`${name}\``).join(", ");
         }
     }
 
@@ -41,7 +54,7 @@ class Command {
         if (message.attachments.first()) {
             // use the helper if they added an attachment (otherwise the helper will use customs
             // when we dont want to, like if we were replying to other users)
-            [customBuffer] = util.getInputImagesForCommand(message);
+            [customBuffer] = await util.getInputImagesForCommand(message);
             if (!customBuffer) return;
         }
 
@@ -51,42 +64,16 @@ class Command {
 
         // if we are using a custo  char, the text color is args[0];
         const characterName = customBuffer ? "custom" : args[0];
-        const characterColor = customBuffer ? args[0] : this.colors[characterName];
-        if (!customBuffer && this.characters.has(characterName))
+        const characterColor = tryCatch(() => cssColor.convert.colorToHex(customBuffer ? args[0] : this.colors[characterName]));
+        if (!customBuffer && !this.characters.has(characterName))
             return message.reply("That's not a valid character. See the help for this command.");
         if (!characterColor)
-            return message.reply("TODO: thiouwehdn");
-        
-        // we just expect this to work because realistically the command shouldnt work if this doesnt
-        const attachmentFetch = await fetch(attachment.url);
-        const attachmentString = await attachmentFetch.text();
-        if (!attachmentString.slice(0, 128).includes("<")) return message.reply("That doesn't look like an SVG.");
+            return message.reply("You need to provide a proper text color for custom images.");
+        args.shift(); // remove the name or color arg
 
-        // attempt rendering
-        try {
-            const repaired = svgRepair(attachmentString);
-            const image = await svgRender(repaired, renderDpi);
-            message.reply({
-                content: `Displayed by <@${message.author.id}>`,
-                files: [image],
-                allowedMentions: {
-                    parse: [],
-                    users: [],
-                    roles: [],
-                    repliedUser: true
-                }
-            });
-        } catch (err) {
-            message.reply({
-                content: `Failed to render, even after repairing your SVG: ${err}`.substring(0, 2000),
-                allowedMentions: {
-                    parse: [],
-                    users: [],
-                    roles: [],
-                    repliedUser: true
-                }
-            });
-        }
+        // render the text we need to draw
+        const textToSay = `${args.join(" ")}`;
+        return message.reply("Not implemented");
     }
 }
 
