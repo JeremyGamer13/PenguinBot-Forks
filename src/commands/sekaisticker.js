b29c6317ed8aa651e5dd34f8e9c8f94d0523bf32
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2026-08-11 22:46:57 -0600

    sekaisticker command from old jg bot

--- src/commands/sekaisticker.js
+++ src/commands/sekaisticker.js
@@ -1,10 +1,12 @@
 const fs = require("fs/promises");
 const path = require("path");
 
+const discord = require("discord.js");
 const cssColor = require("@asamuzakjp/css-color");
 
 const jimp = require("jimp");
 const { Jimp, JimpMime } = jimp;
+const { Canvas, FontLibrary } = require("skia-canvas");
 
 const env = require("../util/env-util.js");
 const tryCatch = require("../util/try-catch.js");
@@ -28,11 +30,6 @@ class Command {
         });
     }
     async initialize() {
-        // load the font fnt
-        // this font should be white & match fontSize. make sure the fontSize is larger than all sticker defaultText sizes
-        this.font = await jimp.loadFont("./assets/sekaisticker/YurukaStd.fnt");
-        this.fontSize = 64;
-
         // initialize the stickers
         const sekaiPath = env.get("SEKAI_STICKERS_PATH");
         this.sekaiPath = sekaiPath;
@@ -49,12 +46,19 @@ class Command {
         this.characters = new Set();
         for (const sticker of stickers) {
             const characterName = `${sticker.character}`.toLowerCase().replace(/\s/g, "").trim();
-            if (this.characters.has(characterName))
-                continue;
-            this.characters.add(characterName);
+            if (!this.characters.has(characterName))
+                this.characters.add(characterName);
             this.colors[characterName] = sticker.color;
-            this.stickers[characterName] = sticker;
+
+            // add sticker to list
+            const stickerList = this.stickers[characterName] || [];
+            stickerList.push(sticker);
+            this.stickers[characterName] = stickerList;
         }
+
+        // load the woff2 fonts
+        FontLibrary.use("YurukaStd", [path.join(sekaiPath, "src/fonts/YurukaStd.woff2")]);
+        FontLibrary.use("SSFangTangTi", [path.join(sekaiPath, "src/fonts/ShangShouFangTangTi.woff2")]);
 
         // make the long description from the list of characters
         this.descriptionLong = "Caption a sticker of a Project SEKAI chibi."
@@ -89,10 +93,78 @@ class Command {
             return message.reply("You need to provide a proper text color for custom images.");
         args.shift(); // remove the name or color arg
 
-        // render the text we need to draw
+        // vaildate the text
         const textToSay = `${args.join(" ")}`;
+
+        // load everything we need to render this stickker
+        const customSticker = {
+            color: characterColor,
+            defaultText: {
+                text: textToSay,
+                x: 148,
+                y: 42,
+                r: -1,
+                s: 47
+            }
+        };
+        const stickersPossible = this.stickers[characterName];
+        const stickerInfo = customBuffer ? customSticker
+            : stickersPossible[Math.round(Math.random() * (stickersPossible.length - 1))];
+        let stickerInputBuffer = customBuffer;
+        if (!stickerInputBuffer)
+            stickerInputBuffer = await fs.readFile(path.join(this.sekaiPath, "public/img/", stickerInfo.img));
         
-        return message.reply("Not implemented");
+        // render the text we need to draw. we make this a big blank space and then crop it later for rotation
+        // https://github.com/TheOriginalAyaka/sekai-stickers/blob/main/src/App.jsx
+        const stickerTextCanvas = new Canvas(296, 256);
+        const stickerTextCtx = stickerTextCanvas.getContext("2d");
+        stickerTextCtx.lineWidth = 9;
+        stickerTextCtx.textAlign = "center";
+        stickerTextCtx.strokeStyle = "white";
+        stickerTextCtx.fillStyle = characterColor;
+
+        // place  & draw the text
+        stickerTextCtx.translate(stickerInfo.defaultText.x, stickerInfo.defaultText.y);
+        stickerTextCtx.rotate(stickerInfo.defaultText.r / 10);
+
+        // since jsb doesnt have precise controls this just does all the font sizing for the user
+        const textSplit = textToSay.split("\n");
+        for (let i = 0, space = 0; i < textSplit.length; i++) {
+            const textLineToSay = textSplit[i];
+
+            // start at 32 and scale down from there
+            let fontSize = 32;
+            stickerTextCtx.font = `${fontSize}px YurukaStd, SSFangTangTi`;
+
+            // scale down, we use stroke width & just arbitraruiily round up to 12 lol
+            const metrics = stickerTextCtx.measureText(textLineToSay);
+            const maxTextWidth = stickerTextCanvas.width - 12;
+            if (metrics.width > maxTextWidth) {
+                const scale = maxTextWidth / metrics.width;
+                fontSize = Math.floor(fontSize * scale);
+            }
+
+            // draw tthat
+            stickerTextCtx.font = `${fontSize}px YurukaStd, SSFangTangTi`;
+            stickerTextCtx.strokeText(textLineToSay, 0, space);
+            stickerTextCtx.fillText(textLineToSay, 0, space);
+            space += fontSize * 1.25;
+        }
+
+        // render the sticker image
+        // other makers seem to use this sizing
+        const stickerTextJimp = await Jimp.fromBuffer(await stickerTextCanvas.toBuffer("png"));
+        const stickerImage = await Jimp.fromBuffer(stickerInputBuffer);
+        stickerImage.contain({ w: 296, h: 256 });
+        stickerImage.composite(stickerTextJimp, 0, 0);
+
+        // Convert the canvas to a Discord attachment
+        const stickerBuffer = await stickerImage.getBuffer(JimpMime.png);
+        const attachment = new discord.MessageAttachment(stickerBuffer, 'sticker.png');
+        message.reply({
+            content: `Generated by <@${message.author.id}>`,
+            files: [attachment]
+        });
     }
 }
 
